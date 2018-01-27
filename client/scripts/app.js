37e692d8c8430566411a41e5299e5937abf07263
Yufan Wang <yufansmail@gmail.com>
2018-01-27 11:08:34 -0800

    CHATTERBOX
    APP.send now sends correctly

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,16 +8,25 @@
 // }
 var userName = window.location.search.split("name=")[1];
 
-
 var friendList = {};
 
 var app = {
-  server: "http://parse.hrr.hackreactor.com/chatterbox/classes/messages",
+  server: "http://parse.hrr.hackreactor.com/chatterbox/classes/messages?order=-createdAt",
   init: function(){
-    $('#messageForm').find('.submit').submit(app.handleSubmit());
+    $('#send').on('click', function(event) {
+      var message = {
+        username: userName,
+        text: $('#message').val(),
+        roomname: $('#roomSelect').val()
+      };
+      $('#message').val('');
+      app.handleSubmit(message);
+      event.preventDefault();
+    });
     $('#chats').find('.username').on('click', app.handleUsernameClick);
 
-    setInterval(function() { app.fetch() }, 2000);
+    setInterval(app.fetch, 2000);
+
   },
   clearMessages: function(){
     $("#chats").remove();
@@ -30,30 +39,30 @@ var app = {
       dataType: "JSON",
       success: function(data) {
         app.clearMessages();
-        data.results.forEach( result => app.renderMessage(result));
+        data.results.forEach( result => app.renderMessage(result) );
       }
     });
   },
-  send: function(message){
-    $.ajax({
-      url: app.server,
-      type: "POST",
-      data: message, //stringify or not
-      contentType: "application/json",
-      success: function(message) {
-        app.renderMessage(message)
-
+  send: function(message) {
+    $.ajax ({
+      url: "http://parse.hrr.hackreactor.com/chatterbox/classes/messages",
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function() {
+        console.log('sent');
+        console.log(message);
       },
       error: function() {
-        console.log("There was an error. ");
+        console.log('error');
       }
     });
   },
-  renderRoom: function(roomname){
+  renderRoom: function(roomname) {
     var room = $('<option value="' + roomname + '">' + roomname + '</option>');
     $("#roomSelect").append(room);
   },
-  renderMessage: function(message){
+  renderMessage: function(message) {
     var container = $("<div class='chat'></div>");
     var user = $("<div class='username'>" + _.escape(message.username) + ": </div>");
     var text = $("<div class='message'>" + _.escape(message.text) + "</div>");
@@ -61,15 +70,10 @@ var app = {
     container.append(text);
     $("#chats").append(container);
   },
-  handleUsernameClick: function(event){
+  handleUsernameClick: function(event) {
 
   },
-  handleSubmit: function(){
-    var message = {};
-    message.username = userName;
-    message.text = $('#message').val();
-    $('#message').val('');
-    message.roomname = $('#roomSelect').val();
+  handleSubmit: function(message){
     app.send(message);
   }
 };
